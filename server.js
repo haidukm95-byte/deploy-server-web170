042c9b33430267a9e68952da41b0c6857e79088f
Marko Haiduk <haidukm95@gmail.com>
2026-06-07 18:31:01 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`run on ${PORT}`));
+
+console.error("Name=>", process.env.NAME);
+
+app.get("/me/:name", (req, res) => {
+  console.error("Name=>", req.params.name);
+  res.json({ msg: `hello from server,  my name is ${req.params.name}` });
+});
+
+app.get("/welcome", (req, res) => {
+  console.error("Welcome=>", process.env.NAME);
+  res.send(`Welcome ${process.env.NAME}`);
+});
